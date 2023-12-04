5351a6cef1c6fbfea7697d47a7317106c83eae5f
Alvaro <alvarofb@alumnos.iesgalileo.es>
2023-12-04 13:34:46 +0100

    Un commit para probar desde eclipse

---
+++ ejerciFactorial/src/ejerciFactorial/app.java
@@ -0,0 +1,18 @@
+package ejerciFactorial;
+import java.util.Scanner;
+
+public class app {
+	public static void main(String[] args) {
+		try (// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in)) {
+			int fact=1;
+			int num=0;
+			System.out.println("Introduce número");
+			num=teclado.nextInt();
+			for (int i=1;i<=num;i++) {
+				fact=fact* i;
+			}
+			System.out.println("El factorial de "+num+" es "+fact);
+		}
+	}
+}
